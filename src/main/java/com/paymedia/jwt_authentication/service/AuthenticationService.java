60367c8374e8109922745d13ab7d1037155ddd9d
Isuru Pradeep <pradeepisuru31@gmail.com>
2025-05-08 17:00:21 +0530

    initial commit

---
+++ src/main/java/com/paymedia/jwt_authentication/service/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.paymedia.jwt_authentication.service;
+
+
+
+import com.paymedia.jwt_authentication.dto.LoginUserDto;
+import com.paymedia.jwt_authentication.dto.RegisterUserDto;
+import com.paymedia.jwt_authentication.entity.User;
+import com.paymedia.jwt_authentication.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = new User();
+        user.setFullName(input.getFullName());
+        user.setEmail(input.getEmail());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
---
+++ src/main/java/com/paymedia/jwt_authentication/service/UserService.java
@@ -0,0 +1,25 @@
+package com.paymedia.jwt_authentication.service;
+
+import com.paymedia.jwt_authentication.entity.User;
+import com.paymedia.jwt_authentication.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
